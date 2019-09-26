765994ab974b910f043a258696b1f26c2eb07cf7
lanhuong <lanhuong@DESKTOP-EGER9U2>
2019-09-26 21:51:02 +0700

    Add project files.

---
+++ WebApplication1/Models/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Account
+    {
+        public string account { get; set; }
+        public string password { get; set;  }
+
+    }
+}
